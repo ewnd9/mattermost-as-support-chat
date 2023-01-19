e8d70f3b614a4dd679256ec9219e8758817cd440
ewnd9 <ewndnine@gmail.com>
2023-01-19 19:56:23 +0700

    add basic auth and posting

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,72 @@
 import React from 'react';
+import axios from 'axios';
+import { WebSocketClient } from '@mattermost/client';
 
-export const App = () => <div>hello</div>
+// TODO: get list from api
+const channelId = 'tqgnf1feqtyutpmzguxhoda9xh';
+const userId = 'qt55opr3pbnpfjakrymfjemkzc';
+
+const apiClient = axios.create({
+  headers: {
+    // 401 without it
+    'x-requested-with': 'XMLHttpRequest',
+  },
+});
+
+(async () => {
+  await login();
+  await initClient();
+})();
+
+export const App = () => <div>hello</div>;
+
+async function login() {
+  if (!localStorage.auth) {
+    const email = window.prompt('email');
+    const password = window.prompt('password');
+
+    await apiClient.post('/api/v4/users/login', {
+      login_id: email,
+      password,
+      token: '',
+      deviceId: '',
+    });
+
+    localStorage.auth = 'true';
+  }
+}
+
+async function initClient() {
+  const client = new WebSocketClient();
+  client.initialize(`ws://${location.host}/api/v4/websocket`);
+
+  client.addMessageListener((...args) =>
+    console.log('addMessageListener', ...args)
+  );
+  client.addFirstConnectListener((...args) =>
+    console.log('addFirstConnectListener', ...args)
+  );
+  client.addReconnectListener((...args) =>
+    console.log('addReconnectListener', ...args)
+  );
+  client.addMissedMessageListener((...args) =>
+    console.log('addMissedMessageListener', ...args)
+  );
+  client.addCloseListener((...args) =>
+    console.log('addCloseListener', ...args)
+  );
+
+  setTimeout(async () => {
+    await apiClient.post('/api/v4/posts', {
+      file_ids: [],
+      message: 'post ' + Date.now(),
+      props: { disable_group_highlight: true },
+      metadata: {},
+      channel_id: channelId,
+      user_id: userId,
+      create_at: 0,
+      update_at: Date.now(),
+      reply_count: 0,
+    });
+  }, 1000);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,27 @@ export default defineConfig({
     //   exclude: ['core-js'],
     // }),
   ],
+  server: {
+    proxy: {
+      // // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      // '/foo': 'http://localhost:4567',
+      // // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+      // '/api': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+      // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+
+      '/api/v4/websocket': {
+        target: 'ws://localhost:8065',
+        ws: true,
+        // changeOrigin: true,
+      },
+      '/api/v4': {
+        target: 'http://localhost:8065',
+        changeOrigin: true,
+      },
+    },
+  },
 });
